4c44f6567d23c8a14ed2e8c9b97a225c443c8241
Lex da Costa <alexfebronio81@gmail.com>
2026-06-08 14:12:31 -0300

    Melhorando o if, adicionando função de retorno e try

--- app.py
+++ app.py
@@ -35,8 +35,14 @@ def finalizar_app():
           
           """)
 
+def opcao_invalida():
+    print('Opção inválida!\n')
+    print('Digite uma tecla para voltar ao menu principal')
+    main()
+
 def escolher_opcao():
-    opcao_escolhida = int(input('Digite a opção desejada: '))
+    try:
+        opcao_escolhida = int(input('Digite a opção desejada: '))
 # outro jeito seria opcao_escolhida = int(opcao_escohida) para converter a string digitada pelo usuário em um número inteiro, mas o jeito mais recomendado é usar o int() para garantir que a variável seja do tipo inteiro.
 
 # tinha isso antes... print(f'Opção escolhida: {opcao_escolhida}') esse é um dos jeitos de interpolar strings, outro seria usar "string"{variavel}"string" e concatenar usando o sinal de +, mas o jeito mais recomendado é usar o f-string, que é mais legível e fácil de usar.
@@ -44,16 +50,22 @@ def escolher_opcao():
 # print(type(opcao_escolhida)) # para verificar o tipo da variável, que é string.
 # print(type(1)) # para verificar o tipo do número 1, que é inteiro.
 
-    if opcao_escolhida == 1:
-        print('Opção 1 escolhida: Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Opção 2 escolhida: Listar restaurante')
-    elif opcao_escolhida == 3:
-        print('Opção 3 escolhida: Ativar restaurante')  
-    else:
-        finalizar_app()
+        if opcao_escolhida == 1:
+            print('Opção 1 escolhida: Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('Opção 2 escolhida: Listar restaurante')
+        elif opcao_escolhida == 3:
+            print('Opção 3 escolhida: Ativar restaurante')  
+        elif opcao_escolhida == 4:
+            print('Opção 4 escolhida: Sair')
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('cls')  # Limpa a tela do terminal
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
